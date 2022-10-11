9c6883244fafb8394a5907c60a23a24e626ece21
lilithering@gmail.com <lilithering@gmail.com>
2022-10-11 15:05:15 -0300

    Input [input.asm] Admin: - Tentativa de uma build para programa que usa o syscall read para ler \ entradas de teclado. - Adicionei uma biblioteca para guardar as algumas definições.

---
+++ input.asm
@@ -0,0 +1,28 @@
+%include 'defines.inc'
+
+section .data
+global main
+    MSG: db 'Selecione uma letra/número', 10
+    LEN: equ $-msg
+
+section .bss
+    %define BUFFER_INPUT 1
+    input: resb BUFFER_INPUT
+
+section .text
+main:
+    mov eax, SYS_WRITE
+    mov ebx, STDOUT
+    mov ecx, MSG
+    mov edx, LEN
+    int 0x80
+
+    mov eax, SYS_READ
+    mov ebx, STDIN
+    mov ecx, input
+    mov edx, BUFFER_INPUT
+    int 0x80
+
+    mov eax, SYS_EXIT
+    mov ebx, EXIT_OK
+    int 0x80
